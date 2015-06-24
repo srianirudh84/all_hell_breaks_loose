2ae02e973ed1dc16cd178ad949f84da4c336dd78
Anirudh Srinivasan <srianirudh@gmail.com>
2015-06-24 12:09:46 -0700

    modified this code

--- pullanddeploy.py
+++ pullanddeploy.py
@@ -25,7 +25,7 @@ def git_reset():
 	GIT_RESET = split(GITRESET)
 	f3 = call(GIT_RESET)
 def rsyn_cmd():
-	RSYNCCMD = '/usr/bin/rsync -vrlptD --delete /opt/www/html/ drt-mkt-webapp-prod-1:/opt/www/html/'
+	RSYNCCMD = '/usr/bin/rsync -vrlptD --delete /opt/www/html/ prod-server:/opt/www/html/'
 	RSYNC_CMD = split(RSYNCCMD)
 	f4 = call(RSYNC_CMD)
 
@@ -35,11 +35,11 @@ if __name__ == '__main__':
     	a = raw_input("Please pick a commit ID: ")
  	git_pull()
 	git_reset()
-	logging.info('COMMIT ID:%s pulled to the staging server drt-mkt-webapp-stage-1' %a)
+	logging.info('COMMIT ID:%s pulled to the staging server stage-server' %a)
 	yes_or_no = raw_input('Do you want to push the COMMIT ID:%s to the Production server ? Type y/n:' %a)
 	if yes_or_no == 'y':
-		logging.info('Pushing the COMMIT ID: %s to the Production server drt-mkt-webapp-prod-1' %a)
-		print 'Pushing the COMMIT ID: %s to the Production server drt-mkt-webapp-prod-1' %a
+		logging.info('Pushing the COMMIT ID: %s to the Production server prod-server' %a)
+		print 'Pushing the COMMIT ID: %s to the Production server prod-server' %a
 		#rsyn_cmd()
 	elif yes_or_no == 'n':
 		 logging.info('Pushed notting to the Production server')
